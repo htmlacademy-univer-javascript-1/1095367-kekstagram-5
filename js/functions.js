18a1f19356beb200d2d0749abf19e461a4301d44
ElenaM <115351366+LenaMishanina@users.noreply.github.com>
2024-12-06 16:40:38 +0300

    impl fun isPalindrome

--- js/functions.js
+++ js/functions.js
@@ -8,9 +8,29 @@ function isLineLengthLessOrEqual(line, maxLength) {
   return line.length <= maxLength;
 }
 
-// Cтрока короче 20 символов
-isLineLengthLessOrEqual('проверяемая строка', 20); // true
-// Длина строки ровно 18 символов
-isLineLengthLessOrEqual('проверяемая строка', 18); // true
-// Строка длиннее 10 символов
-isLineLengthLessOrEqual('проверяемая строка', 10); // false
+console.log(isLineLengthLessOrEqual('проверяемая строка', 20)); // true
+console.log(isLineLengthLessOrEqual('проверяемая строка', 18)); // true
+console.log(isLineLengthLessOrEqual('проверяемая строка', 10)); // false
+
+
+// Функция для проверки, является ли строка палиндромом.
+// Палиндром — это слово или фраза, которые одинаково читаются и слева направо и справа налево.
+
+function isPalindrome(line) {
+  const lowercase = line.toLowerCase().replaceAll(' ', '');
+
+  for (let i = 0; i < lowercase.length / 2 - 1; i++) {
+    if (lowercase[i] !== lowercase[i + lowercase.length - 1 - i * 2]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+console.log('топот', isPalindrome('топот')); // true
+console.log('ДовОд', isPalindrome('ДовОд')); // true
+console.log('Кекс', isPalindrome('Кекс')); // false
+console.log('12321', isPalindrome('12321')); // true
+console.log('кабак', isPalindrome('КаАбак')); // true
+console.log('Лёша на полке клопа нашёл ', isPalindrome('Лёша на полке клопа нашёл ')); // true
