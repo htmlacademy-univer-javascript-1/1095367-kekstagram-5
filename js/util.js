6673ea1d06429819e9a69bf0ea291ac73c239e33
ElenaM <115351366+LenaMishanina@users.noreply.github.com>
2024-12-14 10:42:34 +0300

    split code into modules

---
+++ js/util.js
@@ -0,0 +1,10 @@
+//модуль с вспомогательными функциями;
+
+const getRandomNum = (a, b) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+export {getRandomNum};
